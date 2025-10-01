b9ff00252633d0c6ccd9f9bcf0baaf9012b35e5a
Termins55 <kevgame123@gmail.com>
2025-10-01 21:58:27 +0300

    Add isVisible

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import "./App.css";
-import Counter from "./components/Counter";
-import Header from "./components/Header";
+// import "./App.css";
+// import Counter from "./components/Counter";
+// import Header from "./components/Header";
+import { Component } from "react";
+import StopWatch from "./components/StopWatch";
+import { isVisible } from "@testing-library/user-event/dist/utils";
 
-// Реалізувати компонент header, який містить логотип і
-// або фото користувача, якщо він залогінився
-// або кнопку login/registration
-function App() {
-  const isLogin = true;
-  return (
-    <>
-      {/* <Header isLogin={isLogin} />; */}
-      <Counter/>
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isVisible: false,
+    };
+  }
+  handleClick = () => {
+    const { isVisible } = this.state;
+    this.setState({ isVisible: !isVisible });
+  };
+
+  render() {
+    const { isVisible } = this.state;
+
+    return (
+      <>
+        <button onClick={this.handleClick}>
+          {isVisible ? "Unmount" : "Mount"}
+        </button>
+        {isVisible && <StopWatch />}
+      </>
+    );
+  }
 }
 
 export default App;
