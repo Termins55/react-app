59f22974443e47c67fde5a568900e476145f379b
Termins55 <kevgame123@gmail.com>
2025-10-09 20:57:50 +0300

    Add login form

--- src/App.js
+++ src/App.js
@@ -1,39 +1,7 @@
-// import "./App.css";
-// import Counter from "./components/Counter";
-// import Header from "./components/Header";
-import { Component } from "react";
-// import StopWatch from "./components/StopWatch";
-// import AnimalSlider from "./components/AnimalSlider";
-import UsersList from "./components/UsersList";
-import ThemeButton from "./components/ThemeButton";
-// import ViewPortParams from "./components/ViewPortParams";
+import LoginForm from "./components/LoginForm";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLight: true,
-    };
-  }
-  setTheme = () => {
-    const { isLight } = this.state;
-    this.setState({ isLight: !isLight });
-  };
-  render() {
-    const { isLight } = this.state;
-    return (
-      <>
-        <ThemeButton isLight={isLight} changeTheme={this.setTheme} />
-        <UsersList isLight={isLight} />
-      </>
-    );
-  }
+function App() {
+  return <LoginForm />;
 }
-
-// App;
-//   ThemeButton; напис
-//   UsersList;
-//     UsersListItem; підсвітка
 
 export default App;
---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,102 @@
+import { Component } from "react";
+import classNames from "classnames";
+import styles from "./LoginForm.module.css";
+
+// state -> input
+// state <- input
+//        ^
+//     onChange
+
+const INITIAL_VALUES = { email: "", password: "" };
+
+const LOGIN_FORM_REX_EXP = {
+  email: /^.+@.+$/,
+  password: /^(?=.*[A-Z].*)(?=.*[a-z].*)(?=.*\d.*)(?=.*[!@#$%^&*.].*).{8,20}$/,
+};
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: INITIAL_VALUES.email,
+      isEmailValid: false,
+      password: INITIAL_VALUES.password,
+      isPasswordValid: false,
+    };
+  }
+
+  handleEmailChange = ({ target: { value } }) => {
+    this.setState({
+      email: value,
+      isEmailValid: LOGIN_FORM_REX_EXP.email.test(value),
+    });
+  };
+
+  handlePasswordChange = ({ target: { value } }) => {
+    this.setState({
+      password: value,
+      isPasswordValid: LOGIN_FORM_REX_EXP.password.test(value),
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // send request
+    this.setState(INITIAL_VALUES);
+  };
+
+  render() {
+    // додати поле для пароля (розмітка + обробник)
+    const { email, password, isEmailValid, isPasswordValid } = this.state;
+
+    // const emailClassName = `${styles.input} ${
+    //   isEmailValid ? styles.inputValid : styles.inputInvalid
+    // }`;
+
+    const emailClassName = classNames(styles.input, {
+      [styles.inputValid]: isEmailValid,
+      [styles.inputInvalid]: !isEmailValid,
+    });
+
+    const passwordClassName = classNames(styles.input, {
+      [styles.inputValid]: isPasswordValid,
+      [styles.inputInvalid]: !isPasswordValid,
+    });
+
+    return (
+      <div className={styles.formContainer}>
+        <h1 className={styles.formHeader}>LoginForm</h1>
+        <form className={styles.loginForm} onSubmit={this.handleSubmit}>
+          <label className={styles.label}>
+            <span className={styles.inputName}>Email</span>
+            <input
+              className={emailClassName}
+              type="email"
+              name="email"
+              placeholder="your@mail"
+              value={email}
+              onChange={this.handleEmailChange}
+              autoFocus
+            ></input>
+          </label>
+          <label className={styles.label}>
+            <span className={styles.inputName}>Password</span>
+            <input
+              className={passwordClassName}
+              type="password"
+              name="password"
+              placeholder="password"
+              value={password}
+              onChange={this.handlePasswordChange}
+              autoFocus
+            ></input>
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
