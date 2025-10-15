9e686c2a8ef21ebb2c51ff984a965ee6aca44432
Termins55 <kevgame123@gmail.com>
2025-10-15 19:35:31 +0300

    Add fetching user

--- src/components/UsersLoader/index.jsx
+++ src/components/UsersLoader/index.jsx
@@ -5,20 +5,29 @@ class UsersLoader extends Component {
     super(props);
 
     this.state = {
-      users: [
-        { id: 1, name: "Ivo" },
-        { id: 2, name: "Wally" },
-      ],
+      users: [],
       isFetching: false,
-      error: null, 
+      error: null,
       currentPage: 1,
     };
   }
+
+  componentDidMount() {
+    const { currentPage } = this.state;
+
+    this.setState({ isFetching: true });
+    fetch(`https://randomuser.me/api?results=5&seed=pe2022&page=${currentPage}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ users: data.results }))
+      .catch((e) => this.setState({ error: e }))
+      .finally(() => this.setState({ isFetching: false }));
+  }
+
   render() {
     const { users, isFetching, error } = this.state;
     return (
       <>
-        {error && <div>!!!ERROR!!!</div>}
+        {error && <div>!!!ERROR!!!{JSON.stringify(error)}</div>}
         {isFetching && <div>Loading. Please waite...</div>}
         {!error && !isFetching && (
           <ul>
