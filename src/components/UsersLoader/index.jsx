4be02d8b4213a861de73c24bba26c16c9ae28363
Termins55 <kevgame123@gmail.com>
2025-10-16 20:45:02 +0300

    Add nextPage and prevPage

--- src/components/UsersLoader/index.jsx
+++ src/components/UsersLoader/index.jsx
@@ -12,7 +12,7 @@ class UsersLoader extends Component {
     };
   }
 
-  componentDidMount() {
+  loadUsers = () => {
     const { currentPage } = this.state;
 
     this.setState({ isFetching: true });
@@ -21,7 +21,31 @@ class UsersLoader extends Component {
       .then((data) => this.setState({ users: data.results }))
       .catch((e) => this.setState({ error: e }))
       .finally(() => this.setState({ isFetching: false }));
+  };
+
+  componentDidMount() {
+    this.loadUsers();
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { currentPage } = this.state;
+
+    if (currentPage !== prevState.currentPage) {
+      this.loadUsers();
+    }
+  }
+
+  nextPage = () => {
+    const { currentPage } = this.state;
+    this.setState({ currentPage: currentPage + 1 });
+  };
+
+  prevPage = () => {
+    const { currentPage } = this.state;
+    if (currentPage > 1) {
+      this.setState({ currentPage: currentPage - 1 });
+    }
+  };
 
   render() {
     const { users, isFetching, error } = this.state;
@@ -30,11 +54,15 @@ class UsersLoader extends Component {
         {error && <div>!!!ERROR!!!{JSON.stringify(error)}</div>}
         {isFetching && <div>Loading. Please waite...</div>}
         {!error && !isFetching && (
-          <ul>
-            {users.map((u) => (
-              <li key={u.id}>{JSON.stringify(u)}</li>
-            ))}
-          </ul>
+          <>
+            <button onClick={this.prevPage}>{"<"}</button>
+            <button onClick={this.nextPage}>{">"}</button>
+            <ul>
+              {users.map((u) => (
+                <li key={u.id}>{JSON.stringify(u)}</li>
+              ))}
+            </ul>
+          </>
         )}
       </>
     );
