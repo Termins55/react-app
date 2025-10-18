6d5297172138b352158eab7427ec1e99b742d0eb
Termins55 <kevgame123@gmail.com>
2025-10-18 17:53:51 +0300

    Add NamedList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import AnimalSlider from "./components/AnimalSlider";
-// import UsersLoader from "./components/UsersLoader";
+const NamedList = ({name, children}) => {
+  return (
+    <>
+      <h3>{name}</h3>
+      <ul>{children}</ul>
+    </>
+  );
+};
+
+// реалізувати NamedList: іменований список із заголовком з name
+// та вмістом списку з контенту NamedList
+// Odd numbers: - name
+// 1            - контент
+// 3            - контент
+// 5            - контент
 
 function App() {
-  return <AnimalSlider />;
+  return (
+    <NamedList name="Odd number">
+      <ul>
+        <li>1</li>
+        <li>3</li>
+        <li>5</li>
+      </ul>
+    </NamedList>
+  );
 }
 
 export default App;
