fff1986fe802d56a51d279dbdc5247c0368533d5
Termins55 <kevgame123@gmail.com>
2025-10-18 18:21:47 +0300

    Add ImageWrapperBase

---
+++ src/components/ImageWrapper/index.jsx
@@ -0,0 +1,16 @@
+import styles from "./ImageWrapper.module.sass";
+
+// проброс пропсів
+function ImageWrapper({ width, height, children, ...restProps }) {
+  const wrapperDims = {
+    width, //width: width,
+    height, // height: height,
+  };
+  return (
+    <div {...restProps} style={wrapperDims} className={styles.imageContainer}>
+      {children}
+    </div>
+  );
+}
+
+export default ImageWrapper;
