9a5a08a8667973f7772933b227a58a76a7246f84
Termins55 <kevgame123@gmail.com>
2025-10-26 16:04:39 +0200

    Add renderProps App jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import DataProvider from './components/DataProvider/index';
+import DataProvider from "./components/DataProvider/index";
 
-const PhonesLoadedList = props => {
+const PhonesLoadedList = (props) => {
   const { data: phones, isFetching, error } = props.phonesData;
   return (
     <>
       {isFetching && <div>Loading...</div>}
       {error && <div>ERROR</div>}
       <ol>
-        {phones.map(p => (
+        {phones.map((p) => (
           <li key={p.id}>
             {p.brand} {p.model}: {p.price}
           </li>
@@ -19,3 +19,17 @@ const PhonesLoadedList = props => {
 
 // Рендер-проп - функція, яка повідомляє компоненту що необхідно відрендерити
 // Цей патерн дозволяє ділитися логікою
+
+function App() {
+  return (
+    <>
+      {/* <DataProvider
+        fileName="/data/phones.json"
+        render={data => <PhonesLoadedList phonesData={data} />}
+      /> */}
+      <DataProvider fileName="/data/phones.json">
+        {(data) => <PhonesLoadedList phonesData={data} />}
+      </DataProvider>
+    </>
+  );
+}
