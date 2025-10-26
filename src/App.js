5f9ba91708554b7a32e1b5ec8f10aad672f7287d
Termins55 <kevgame123@gmail.com>
2025-10-26 11:41:44 +0200

    Add Calendar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import ImageWrapper from "./components/ImageWrapper";
+import {
+  getDate,
+  getDay,
+  startOfMonth,
+  startOfWeek,
+  endOfMonth,
+  getWeek,
+  getYear,
+  addDays,
+  parse,
+  isEqual,
+  isSameMonth,
+} from "date-fns";
 
-// Проп children дозволяє ділитися версткою і стилями
+import Calendar from './components/Calendar/index'
+
+const WEEK_DAYS = ["SANDAY", "M...", "T", "W", "T", "F", "S"];
+
 function App() {
-  return (
-    <ImageWrapper
-      // width="400px"
-      // height="300px"
-      title="image"
-      tabIndex={1}
-      className="container"
-    >
-      <img
-        src="https://images.pexels.com/photos/11046405/pexels-photo-11046405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="home"
-      />
-    </ImageWrapper>
-  );
+  const currentDate = new Date();
+  const date = new Date();
+
+  console.log("date :>> ", getDate(currentDate));
+  console.log("day :>> ", WEEK_DAYS[getDay(currentDate)]);
+
+  return <Calendar />;
 }
 
 export default App;
