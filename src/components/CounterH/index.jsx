f88373fb5dc699829ecae0b4ab59375f425f6f7e
Termins55 <kevgame123@gmail.com>
2025-10-30 19:58:11 +0200

    Add counter with state hook

---
+++ src/components/CounterH/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function CounterH() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const increment = () => {
+    setCount(count + step);
+  };
+
+  // decrement
+
+  const decrement = () => {
+    setCount(count - step);
+  };
+
+  handleStepChange = ({ target: { value } }) => {
+    setStep(Number(value));
+  };
+  return (
+    <div>
+      {count}
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+      <input type="number" value={step} onChange={handleStepChange} />
+    </div>
+  );
+}
+
+export default CounterH;
