afc40630a7ac6b332140b4974fd84b2e00ce81ec
Termins55 <kevgame123@gmail.com>
2025-12-18 19:25:57 +0200

    Add NotFound

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
 import { useEffect } from "react";
-import CounterH from "./components/CounterH";
-import StopWatchH from "./components/StopWatchH";
-
 import {
   Link,
   Route,
   BrowserRouter as Router,
-  Switch,
-  useHistory,
-  useRouteMatch,
-} from "react-router-dom/cjs/react-router-dom.min";
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
-export default function App() {
+{
+  /* <Route path="" Component={About} />; */
+}
+
+function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/components">Components</Link>
-          </li>
-          <li>
-            <Link to="/contacts">Contacts</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/components">
-          <Components />
-        </Route>
-        <Route path="/contacts">
-          <Contacts />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
+  );
+}
+// Link змінює location (host:port/location)
+// Router підписаний на зміну location
+// Коли location змінюється, то Router шукає відповідрний Router
+// і виводе з нього element / Component
+export default App;
+
+function NotFound() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const id = setTimeout(() => {
+      navigate("/");
+    }, 2000);
+    return () => {
+      clearTimeout(id);
+    };
+  });
+  return (
+    <div>
+      404 This page is not exist yet
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+    </div>
   );
 }
 
@@ -57,44 +57,4 @@ function Home() {
 
 function About() {
   return <div>About</div>;
-}
-
-function Components() {
-  const { path, url } = useRouteMatch();
-  return (
-    <div>
-      <ol>
-        <li>
-          <Link to={`${url}/counterh`}>CounterH</Link>
-        </li>
-        <li>
-          <Link to={`${url}/stopwatchh`}>SwopWatchH</Link>
-        </li>
-      </ol>
-      <Switch>
-        <Route path={`${path}/counterh`}>
-          <CounterH />
-        </Route>
-        <Route path={`${path}/stopwatchh`}>
-          <StopWatchH />
-        </Route>
-        <Route path={`${path}/*`}>
-          <NotFound />
-        </Route>
-      </Switch>
-    </div>
-  );
-}
-
-function Contacts() {
-  return <div>Contacts</div>;
-}
-
-function NotFound() {
-  const history = useHistory();
-
-  useEffect(() => {
-    setTimeout(() => history.push("/"), 5000);
-  });
-  return <div>404 Not Found</div>;
 }
