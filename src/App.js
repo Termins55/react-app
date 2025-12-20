2566911a1ee1e09b87466f1ebd24f21e29fbb4c6
Termins55 <kevgame123@gmail.com>
2025-12-20 14:07:57 +0200

    Outlet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import {
   Link,
+  NavLink,
+  Outlet,
   Route,
   BrowserRouter as Router,
   Routes,
@@ -11,16 +13,19 @@ import {
   /* <Route path="" Component={About} />; */
 }
 
+const linkStyle = ({ isActive }) =>
+  isActive ? { border: "1px solid purple" } : {};
+
 function App() {
   return (
     <Router>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<BasePage />}>
+          <Route index path="/" element={<Home />} />
+          <Route path="/partners/*" element={<PartnersPage />}></Route>
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </Router>
   );
@@ -30,6 +35,35 @@ function App() {
 // Коли location змінюється, то Router шукає відповідрний Router
 // і виводе з нього element / Component
 export default App;
+
+function Header() {
+  return (
+    <header>
+      <h1>My App</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" style={linkStyle}>
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/partners" style={linkStyle}>
+              Partners
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/about" style={linkStyle}>
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
 function NotFound() {
   const navigate = useNavigate();
@@ -48,6 +82,40 @@ function NotFound() {
         <Link to="/">Home</Link>
       </div>
     </div>
+  );
+}
+
+function PartnersPage() {
+  // /partners/partners1
+  // /partners/partners2
+
+  return (
+    <div>
+      <h1>Partners</h1>
+      <nav>
+        <NavLink to="partner1" style={linkStyle}>
+          Partner 1
+        </NavLink>
+        <NavLink to="partner2" style={linkStyle}>
+          Partner 2
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route path="partner1" element={<div>Partner 1</div>} />
+        <Route path="partner2" element={<div>Partner 2</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+function BasePage() {
+  return (
+    <>
+      <Header />
+      {/* вбудуй що потрібно */}
+      <Outlet />
+      <footer>Footer</footer>
+    </>
   );
 }
 
