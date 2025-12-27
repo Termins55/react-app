7e3114b63b87b1f56de10a01fc85a455771d7ce6
Termins55 <kevgame123@gmail.com>
2025-12-27 18:51:37 +0200

    Refactor ContactForm

--- src/components/forms/ContactForm/index.jsx
+++ src/components/forms/ContactForm/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { CONTACT_VALIDATION_SCHEMA } from "../../../utils/validate/validationSchemas";
 import styles from "./ContactForm.module.sass";
 import classNames from "classnames";
+import Input from "../Input";
 
 // name,tel,email,birthday
 
@@ -13,6 +14,13 @@ function ContactForm() {
     console.log("Submit form with values: :>> ", values);
     formikBag.resetForm();
   };
+
+  const classes = {
+    error: styles.error,
+    input: styles.input,
+    valid: styles.valid,
+    invalid: styles.invalid,
+  };
   return (
     // initialValues onSubmit - обовʼязкові пропи для Formik
     <Formik
@@ -20,73 +28,37 @@ function ContactForm() {
       onSubmit={handleSubmit}
       validationSchema={CONTACT_VALIDATION_SCHEMA}
     >
-      {(formikProps) => {
-        const nameClassNames = classNames(styles.input, {
-          [styles.valid]: !formikProps.errors.name && formikProps.touched.name,
-          [styles.invalid]: formikProps.errors.name && formikProps.touched.name,
-        });
-
-        const phoneClassNames = classNames(styles.input, {
-          [styles.valid]:
-            !formikProps.errors.phone && formikProps.touched.phone,
-          [styles.invalid]:
-            formikProps.errors.phone && formikProps.touched.phone,
-        });
-
-        // other Fiels
-        return (
-          <Form className={styles.form}>
-            <label>
-              <span>Name</span>
-              <Field
-                className={nameClassNames}
-                type="text"
-                name="name"
-                placeholder="Yourname"
-                autoFocus
-              />
-              <ErrorMessage
-                className={styles.error}
-                name="name"
-                component="span"
-              />
-            </label>
-            <label>
-              <span>Phone Number:</span>
-              <Field
-                className={phoneClassNames}
-                type="tel"
-                name="phone"
-                placeholder="+380111111111"
-              />
-              <ErrorMessage
-                className={styles.error}
-                name="phone"
-                component="span"
-              />
-            </label>
-            <label>
-              <span>Email:</span>
-              <Field type="email" name="email" placeholder="your@email.com" />
-              <ErrorMessage
-                className={styles.error}
-                name="email"
-                component="span"
-              />
-            </label>
-            <label>
-              <span>Birthday:</span>
-              <Field type="date" name="birthday" />
-              <ErrorMessage
-                className={styles.error}
-                name="birthday"
-                component="span"
-              />
-            </label>
-            <button type="submit">Save</button>
-          </Form>
-        );
-      }}
+      <Form className={styles.form}>
+        <Input
+          label="Name:"
+          type="text"
+          name="name"
+          placeholder="Yourname"
+          autoFocus
+          classes={classes}
+        />
+        <Input
+          label="Phone Number:"
+          type="tel"
+          name="phone"
+          placeholder="+380111111111"
+          classes={classes}
+        />
+        <Input
+          label="Email:"
+          type="email"
+          name="email"
+          placeholder="your@email.com"
+          classes={classes}
+        />
+        <Input
+          label="Birthday:"
+          type="date"
+          name="birthday"
+          classes={classes}
+        />
+        <button type="submit">Save</button>
+      </Form>
     </Formik>
   );
 }
